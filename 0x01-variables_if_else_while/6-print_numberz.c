b8bd3458ca4dcf01051bbb1ac00c74ccacca6b10
Shizzle21 <Nanansude330@gmail.com>
2022-07-05 00:59:50 -0700

    Print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+* main - entry point
+*
+* Return:always return 0
+*
+*/
+int main(void)
+{
+int d;
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+putchar('\n');
+return (0);
+}

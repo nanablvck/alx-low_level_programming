fc10d76da57be820afbaffc8efb58b2a1336aee1
Shizzle21 <Nanansude330@gmail.com>
2022-07-05 01:10:25 -0700

    Reverse Alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+* main - entry point
+*
+* Return:always return 0
+*
+*/
+int main(void)
+{
+char low;
+for (low = 'z'; low >= 'a'; low++)
+putchar(low);
+putchar('\n');
+return (0);
+}

2f8099d681872038fbc79163723a7a6968596603
Shizzle21 <Nanansude330@gmail.com>
2022-07-04 17:44:00 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+* main - entry point
+* Return:always return 0
+*
+*/
+int main(void)
+{
+char low, e, q;
+e = 'e';
+q = 'q';
+for (low = 'a'; low <= 'z'; low++)
+{
+if (low != e && low != q)
+putchar(low);
+}
+putchar('\n');
+return (0);
+}

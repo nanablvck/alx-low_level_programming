634bef2816f04ed67734acd9acfeaf033ccc597c
Shizzle21 <nanansude330@gmail.com>
2022-09-22 09:03:17 -0700

    print more

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_most_numbers - code to print for digits
+*
+* Return: Always 0
+*/
+void print_most_numbers(void)
+{
+int c;
+for (c = '0'; c <= '9'; c++);
+{
+if (c != '2' && c != '4')
+{
+putchar(c);
+}
+}
+putchar('\n');
+}

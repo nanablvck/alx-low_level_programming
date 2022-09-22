3b9fb733f1338dd45865466f659b2efc32a38343
shizzle21 <nanansude330@gmail.com>
2022-09-22 00:18:14 -0700

    lower

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
-*_isalpha - Check for alphabet
+* _isalpha - Check for alphabet
 * @c: int for the argument of the function
 * Return: Always 0
 */
 int _isalpha(int c)
 {
-if ((c >= 'A' && c <= 'Z' || (c >= 'a' && c <= 'z'))
+if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 {
 return (1);
 }

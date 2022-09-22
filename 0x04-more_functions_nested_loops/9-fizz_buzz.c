f288eaed9bb3a7fef678446558388cd4eac52478
Shizzle21 <nanansude330@gmail.com>
2022-09-22 11:17:24 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* main - code to print for digits
+*
+* Return: Always 0
+*/
+int main(void)
+{
+int c;
+char f[] = "Fizz";
+char b[] = "Buzz";
+char fb[] = "FizzBuzz";
+
+for (c = 1; c <= 100; c++)
+{
+if (c == 100)
+{
+printf("%s", b);
+}
+else if (c % 3 == 0 && c % 5 == 0)
+{
+printf("%s ", fb);
+}
+else if( c % 3 == 0)
+{
+printf("%s ", f);
+}
+else if (c % 5 == 0)
+{
+printf("%s ", b);
+}
+else
+printf("%d ", c);
+}
+printf("\n");
+return (0);
+}
+

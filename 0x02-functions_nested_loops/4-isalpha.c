90c58ea43023da1a7977a4f3a29191f7fc0e757c
shizzle21 <nanansude330@gmail.com>
2022-09-22 00:14:55 -0700

    lower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*_isalpha - Check for alphabet
+* @c: int for the argument of the function
+* Return: Always 0
+*/
+int _isalpha(int c)
+{
+if ((c >= 'A' && c <= 'Z' || (c >= 'a' && c <= 'z'))
+{
+return (1);
+}
+else
+return (0);
+}
